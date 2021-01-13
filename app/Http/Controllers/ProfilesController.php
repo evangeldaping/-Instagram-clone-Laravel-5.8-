aa2718e7a0b0bb4c4b6ca9d4672ee0a259f05eb7
evangel <evangeldaping@gmail.com>
2021-01-14 00:41:39 +0800

    RESTful Resource Controller

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
 {
-    public function index()
+    public function index($user)
     {
         return view('home');
     }
