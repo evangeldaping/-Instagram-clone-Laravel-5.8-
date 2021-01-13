f88d5801f5b444d3554ffd89e8dbc9d7f62113c7
evangel <evangeldaping@gmail.com>
2021-01-13 23:46:54 +0800

    Designing the UI from Instagram

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,34 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fmnl17-2.fna.fbcdn.net/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=instagram.fmnl17-2.fna.fbcdn.net&_nc_ohc=Sc96ZJth3ssAX8kSNL_&tp=1&oh=688afe659b3f7bb5d760a6f4346885a7&oe=60283EE7" class="rounded-circle">
+        </div>
+        <div class="col-9">
+            <div><h1>freecodecamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23l</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
             </div>
+            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+            <div><a href="#">www.freecodecamp.org</a></div>
+        </div>
+    </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://instagram.fcrk1-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c2.0.823.823a/s640x640/137282729_891157201697073_3007940832466214946_n.jpg?_nc_ht=instagram.fcrk1-2.fna.fbcdn.net&_nc_cat=109&_nc_ohc=1rVAeo8JUScAX9EPvH-&tp=1&oh=baef8ff6df810f516c07cd61b19c3297&oe=6029D70E" class="w-100">
+        </div>
+
+        <div class="col-4">
+            <img src="https://instagram.fcrk1-1.fna.fbcdn.net/v/t51.2885-15/e35/c139.0.550.550a/135485859_201650958336080_4564845623124856800_n.jpg?_nc_ht=instagram.fcrk1-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=Uu6-QFFKs2MAX-2dG_t&tp=1&oh=e43ce3e680980c6ab98fa795ccf669db&oe=60273ADC" class="w-100">
+        </div>
+
+        <div class="col-4">
+            <img src="https://instagram.fcrk1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c53.0.900.900a/s640x640/134235244_250072063131384_630243361121788782_n.jpg?_nc_ht=instagram.fcrk1-1.fna.fbcdn.net&_nc_cat=100&_nc_ohc=RC1pp5SRlDoAX9wDZY0&tp=1&oh=163a1392d58b0e495ed0af675ffc80c0&oe=602A6CF3" class="w-100">
         </div>
     </div>
 </div>
