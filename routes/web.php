d765111f45ea582cdd1061a84e43e1fa74624b8b
evangel <evangeldaping@gmail.com>
2021-01-14 20:20:51 +0800

    Showing Posts from Profiles The User Is Following

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::post('follow/{user}', 'FollowsController@store');
 
+Route::get('/', 'PostsController@index');
 Route::get('/p/create', 'PostsController@create');
-Route::get('/p/{post}', 'PostsController@show');
 Route::post('/p', 'PostsController@store');
+Route::get('/p/{post}', 'PostsController@show');
 
 Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
 Route::get('/profile/{user}/edit', 'ProfilesController@edit')->name('profile.edit');
